039d23bfd5155568173c163937f4c8336beaee7a
leiming5 <leiming5@lenovo.com>
2021-04-19 10:12:17 +0800

    update first

---
+++ src/main/java/com/leinovo/npp/NppApplication.java
@@ -0,0 +1,13 @@
+package com.leinovo.npp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NppApplication.class, args);
+    }
+
+}
